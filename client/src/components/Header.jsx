e56dcdd36aaf3042ca4048540ac307e00dcf3c7c
anurag-bhujbal <anuragbhujbal13@gmail.com>
2024-03-25 11:36:10 +0530

    Updated Header and made profile page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import { FaSearch } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header className='bg-slate-200 shadow-md'>
     <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -12,7 +14,7 @@ export default function Header() {
         </h1>
       </Link>
       <form
-        className='bg-slate-100 p-3 rounded-lg flex items-center'
+        className='bg-slate-100 p-3 rounded-lg flex gap-3 items-center'
       >
         <input
           type='text'
@@ -35,10 +37,16 @@ export default function Header() {
             About
           </li>
         </Link>
-        <Link to='/sign-in'>
-          
-            <li className=' text-slate-700 hover:underline'> Sign in</li>
-          
+        <Link to='/profile'>
+        {currentUser ? (
+              <img
+                className='rounded-full h-8 w-8 object-cover '
+                src={currentUser.avatar}
+                alt='profile'
+              />
+            ) : (
+              <li className=' text-slate-700 hover:underline'> Sign in</li>
+            )}
         </Link>
       </ul>
     </div>
